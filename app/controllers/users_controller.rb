c54e4e746c4c4375757b7e2e8cdea1428b829c38
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-03 12:29:21 +0900

    [finish]2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  before_action :baria_user, only: [:edit, :update]
   before_action :authenticate_user!
-
+  before_action :baria_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -15,13 +14,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "successfully updated user!"
+      redirect_to user_path(@user.id), notice: "successfully updated user!"
     else
       render "edit"
     end
